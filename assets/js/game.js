ffddaedbb68622f7fd3854fdd0f2deea72fd1682
Craig <97862337+cbarber1984@users.noreply.github.com>
2022-06-05 18:21:31 -0400

    stored player name as variable

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert! JavaScript is running!")
+// announce the beginning of the fight
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
+
+// collect robot name from player
+var playerName = window.prompt("What is your robot's name?");
+window.alert("Welcome to the battle dome " + playerName + "!");
+console.log("playerName = " + playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
